bd5399a7a27e435adf07ce995231cc875985b718
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-03-05 21:02:59 -0600

    tried to set up types

---
+++ sketch/script.js
@@ -0,0 +1,19 @@
+/// @ts-check
+/// <reference path="../types/p5/global.d.ts" />
+
+const p5 = require("../types/p5");
+
+function setup() {
+  sin(5);
+
+}
+
+/**
+ *
+ * @param {p5.Vector} a
+ */
+function foo(a) {
+}
+function draw() {
+  ellipse(50, 50, 80, 80);
+}
